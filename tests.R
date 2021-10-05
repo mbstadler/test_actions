914febb9b6d2b3b8afa54ebdfcaa7fa9360ef5c9
mbstadler <stadler.michael@gmail.com>
2021-10-05 11:54:08 +0200

    as newlines

--- tests.R
+++ tests.R
@@ -8,19 +8,19 @@ clObj <- parallel::makeCluster(2L)
 
 # digest clObj
 clparam <- getListOfBiocParallelParam(clObj = clObj)
-cat("clparam:")
+cat("\n\nclparam:\n")
 print(clparam)
 
 nworkers <- unlist(lapply(clparam, BiocParallel::bpworkers))
-cat("nworkers: ", nworkers)
+cat("\n\nnworkers: ", nworkers, "\n")
 
 # will use only one layer of parallelization, select best
 clsel <- which.max(nworkers)
-cat("clsel: ", clsel)
+cat("\n\nclsel: ", clsel, "\n")
 
 # use it in bplapply
 ret <- BiocParallel::bplapply(seq.int(nworkers[clsel]),
                               function(i) i,
                               BPPARAM = clparam[[clsel]])
-cat("bplapply ret:")
+cat("\n\nbplapply ret:\n")
 print(ret)
